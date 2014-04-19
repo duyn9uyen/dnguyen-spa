0c5e11100353427887aa12664c11f332fa502ae3
Duy on Captech Laptop <duy.nguyen@alumni.pitt.edu>
2014-04-19 11:01:02 -0400

    changed click area for banner

--- Dnguyen-SPA/Scripts/_site.js
+++ Dnguyen-SPA/Scripts/_site.js
@@ -1,23 +1,28 @@
 ﻿//This script is for the expandable banner. We use css3 classes to expand.
 
 $(function () {
-    var clickArea = $('#banner-container');
-    clickArea.on('click', function () {
-        var $div = $('.expandableDiv');
-        var expand = 'expandableDiv-expand';
-        var collapse = 'expandableDiv-collapse';
+    
+    // We have to wait 1 second because we are using underscore html templates to render the content. If we don't wait,
+    // this script will execute before the css class has been written to the dom so it won't work.
+    setTimeout(function () {
+        var clickArea = $('.clickExpandArea');
+        clickArea.on('click', function () {
+            var $div = $('.expandableDiv');
+            var expand = 'expandableDiv-expand';
+            var collapse = 'expandableDiv-collapse';
 
-        // collapse
-        if ($div.hasClass(expand)) {
-            $div.removeClass(expand);
-            $div.addClass(collapse);
-            $('.makeHidden').hide(0);
-        }
-        else { //expand
-            $div.removeClass(collapse);
-            $div.addClass(expand);
-            $('.makeHidden').delay(500).show(0);
-        }
-
-    });
+            // collapse
+            if ($div.hasClass(expand)) {
+                $div.removeClass(expand);
+                $div.addClass(collapse);
+                $('.makeHidden').hide(0);
+            }
+            else { //expand
+                $div.removeClass(collapse);
+                $div.addClass(expand);
+                $('.makeHidden').delay(500).show(0);
+            }
+        });
+    }, 1000);
+    
 });
